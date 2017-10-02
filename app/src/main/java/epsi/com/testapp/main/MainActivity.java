8d99de8199afcb6b333926127ce0e49fec49ffd4
Matthieu Lagast <mlagast@ikomobi.com>
2017-10-02 18:02:37 +0200

    Feature ex1 - Login with LinearLayout

--- app/src/main/java/epsi/com/testapp/main/MainActivity.java
+++ app/src/main/java/epsi/com/testapp/main/MainActivity.java
@@ -1,11 +1,17 @@
 package epsi.com.testapp.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import epsi.com.testapp.R;
+import epsi.com.testapp.ex1.Ex1LinearActivity;
+
+import static android.R.attr.start;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MainMenuItem[] items = new MainMenuItem[] {
-                new MainMenuItem("Ex 1 - LinearLayout", MainActivity.class),
+                new MainMenuItem("Ex 1 - LinearLayout", Ex1LinearActivity.class),
                 new MainMenuItem("Ex 1 - LinearLayout", MainActivity.class),
                 new MainMenuItem("Ex 1 - LinearLayout", MainActivity.class)
         };
@@ -26,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MainMenuAdapter(this, items);
         menuListView = (ListView) findViewById(R.id.main_list_view);
         menuListView.setAdapter(adapter);
+
+        menuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MainMenuItem item = (MainMenuItem) adapterView.getItemAtPosition(i);
+                Intent intent = new Intent(getApplicationContext(), item.destinationClass);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
